df69de2088cc3c6e0a44191eb58a91114c9a7b05
AlexGookin <165595232+AlexGookin@users.noreply.github.com>
2025-02-11 14:54:45 -0500

    more book collection stuff

--- model/BookCollection.java
+++ model/BookCollection.java
@@ -1,8 +1,38 @@
+// system imports
+import java.util.Properties;
 import java.util.Vector;
+import javafx.scene.Scene;
 
-public class BookCollection {
+// project imports
+import exception.InvalidPrimaryKeyException;
+import event.Event;
+import database.*;
+
+import impresario.IView;
+
+import userinterface.View;
+import userinterface.ViewFactory;
+
+
+public class BookCollection extends EntityBase implements IView{
+
+    private static final String myTableName = "Book";
+    private Vector<Book> books;
+
     // Constructor
-    public BookCollection() {
+    public BookCollection(BookHolder books) throws Exception {
+        super(myTableName);
+        if (books == null) {
+            new Event(Event.getLeafLevelClassName(this), "<init>", "Missing book holder information", Event.FATAL);
+            throw new Exception("UNEXPECTED ERROR: BookCollection.<init>: book holder information is null");
+        }
+        // Unsure about key 
+        String bookHolderId = (String)books.getState("ID");
+        if (bookHolderId == null) {
+            
+        }
+        
+
         Vector<Book> bookList = new Vector<Book>();
     }
 
